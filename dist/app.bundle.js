ba4bc3729cd46c11671a90916a39086b9dee310a
GaelCoat <zoul.gaelcoat@gmail.com>
2017-01-27 13:59:32 +0100

    hey

--- dist/app.bundle.js
+++ dist/app.bundle.js
@@ -9,7 +9,7 @@ webpackJsonp([0],[
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
-	/* WEBPACK VAR INJECTION */(function(Backbone, q, $) {
+	/* WEBPACK VAR INJECTION */(function(Backbone, $, q) {
 	var Main = Backbone.View.extend({
 
 
@@ -18,11 +18,50 @@ webpackJsonp([0],[
 	  },
 
 
+	  renderParticle: function() {
+
+	    var scene = this.$el.find('#particles');
+	    var elem = $('<i>');
+	    var maxWidth = scene.width();
+	    var maxHeight = scene.height();
+	    var size = Math.floor((Math.random() * 20) + 10);
+
+	    // Type picker
+	    var possibleTypes = ['circle', 'cross'];
+	    var type = possibleTypes[Math.floor((Math.random() * 2) + 0)];
+
+	    // Color picker
+	    var possibleColors = ['blue', 'red', 'grey'];
+	    var color = possibleColors[Math.floor((Math.random() * 3) + 0)];
+
+	    elem.attr('class', type+' '+color);
+	    elem.css({
+	      'top': Math.random()*maxHeight+'px',
+	      'left': Math.random()*maxWidth+'px',
+	      'width': size,
+	      'height': size
+	    });
+
+	    scene.append(elem);
+
+	    elem.show(0).addClass('ready').delay(1700).queue(function(done) {
+
+	      elem.remove();
+	      done();
+	    });
+
+	    setTimeout(this.renderParticle.bind(this), 300);
+	    return this;
+	  },
+
 	  render: function() {
+
 
 	    var that = this;
 
 	    return q.fcall(function() {
+
+	      that.renderParticle();
 
 	    });
 
@@ -34,7 +73,7 @@ webpackJsonp([0],[
 	View.render();
 
 
-	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(2), __webpack_require__(5), __webpack_require__(4)))
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(2), __webpack_require__(4), __webpack_require__(5)))
 
 /***/ }
 ]);
--- src/main.js
+++ src/main.js
@@ -7,11 +7,50 @@ var Main = Backbone.View.extend({
   },
 
 
+  renderParticle: function() {
+
+    var scene = this.$el.find('#particles');
+    var elem = $('<i>');
+    var maxWidth = scene.width();
+    var maxHeight = scene.height();
+    var size = Math.floor((Math.random() * 20) + 10);
+
+    // Type picker
+    var possibleTypes = ['circle', 'cross'];
+    var type = possibleTypes[Math.floor((Math.random() * 2) + 0)];
+
+    // Color picker
+    var possibleColors = ['blue', 'red', 'grey'];
+    var color = possibleColors[Math.floor((Math.random() * 3) + 0)];
+
+    elem.attr('class', type+' '+color);
+    elem.css({
+      'top': Math.random()*maxHeight+'px',
+      'left': Math.random()*maxWidth+'px',
+      'width': size,
+      'height': size
+    });
+
+    scene.append(elem);
+
+    elem.show(0).addClass('ready').delay(1700).queue(function(done) {
+
+      elem.remove();
+      done();
+    });
+
+    setTimeout(this.renderParticle.bind(this), 300);
+    return this;
+  },
+
   render: function() {
+
 
     var that = this;
 
     return q.fcall(function() {
+
+      that.renderParticle();
 
     });
 
